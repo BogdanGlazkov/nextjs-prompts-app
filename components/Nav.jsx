69cf4013c043ee8fc1480cee5a1e1583ba3f64f5
BogdanGlazkov <bglazkov@i.ua>
2023-07-15 22:56:14 +0300

    Add navigation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import logoIcon from "@public/assets/images/logo.svg";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    init();
+  }, []);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          className="object-contain"
+          src={logoIcon}
+          alt="Logo"
+          width={30}
+          height={30}
+        />
+        <p className="logo_text">Prompts App</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className="hidden sm:flex">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button className="outline_btn" type="button" onClick={signOut}>
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                className="rounded-full"
+                src={logoIcon}
+                alt="Profile"
+                width={37}
+                height={37}
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  className="black_btn"
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
