5618d43fb3d39ac63ac76ccb2ec758f98b1eca10
BogdanGlazkov <bglazkov@i.ua>
2023-07-17 22:14:49 +0300

    Add Profile component

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
 
@@ -24,7 +25,7 @@ const MyProfile = () => {
 
   return (
     <Profile
-      name="MyProfile"
+      name="My"
       desc="Welcome to your personalized profile page"
       data={posts}
       handleEdit={handleEdit}
